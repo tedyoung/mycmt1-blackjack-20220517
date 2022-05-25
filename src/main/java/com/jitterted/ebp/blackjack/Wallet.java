31ad56ac6e59b66ee5405b48e4075dea2d636f6d
Ted M. Young <tedyoung@gmail.com>
2022-05-25 09:57:08 -0700

    TDD: introduced field for balance

--- src/main/java/com/jitterted/ebp/blackjack/Wallet.java
+++ src/main/java/com/jitterted/ebp/blackjack/Wallet.java
@@ -3,9 +3,11 @@ package com.jitterted.ebp.blackjack;
 public class Wallet {
 
     private boolean isEmpty;
+    private int balance;
 
     public Wallet() {
         isEmpty = true;
+        balance = 0;
     }
 
     public boolean isEmpty() {
@@ -17,6 +19,6 @@ public class Wallet {
     }
 
     public int balance() {
-        return 0;
+        return balance;
     }
 }
