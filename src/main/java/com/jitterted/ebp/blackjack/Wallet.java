3bf7dfad0c4964cc4e4f5d51289e598ecb0c15e8
Ted M. Young <tedyoung@gmail.com>
2022-05-25 09:44:41 -0700

    TDD: new wallet is empty now passes

---
+++ src/main/java/com/jitterted/ebp/blackjack/Wallet.java
@@ -0,0 +1,9 @@
+package com.jitterted.ebp.blackjack;
+
+public class Wallet {
+
+    public boolean isEmpty() {
+        return true;
+    }
+
+}
---
+++ src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
@@ -0,0 +1,17 @@
+package com.jitterted.ebp.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class WalletTest {
+
+    @Test
+    public void newWalletIsEmpty() throws Exception {
+        Wallet wallet = new Wallet();
+
+        assertThat(wallet.isEmpty())
+                .isTrue();
+    }
+
+}
