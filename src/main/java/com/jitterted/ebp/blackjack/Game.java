2995d5511cb190f33c948e9c17c41dd51a24b61c
Ted M. Young <tedyoung@gmail.com>
2022-05-25 12:23:27 -0700

    LAB #9: Player wins Blackjack scenario works

--- src/main/java/com/jitterted/ebp/blackjack/Game.java
+++ src/main/java/com/jitterted/ebp/blackjack/Game.java
@@ -203,6 +203,10 @@ public class Game {
     public void playerPushes() {
         playerBalance += playerBetAmount;
     }
+
+    public void playerWinsBlackjack() {
+        playerBalance += playerBetAmount * 2.5;
+    }
 }
 
 
--- src/test/java/com/jitterted/ebp/blackjack/GameBetPayoffTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/GameBetPayoffTest.java
@@ -72,6 +72,17 @@ public class GameBetPayoffTest {
                 .isEqualTo(75 - 40 + 40);
     }
 
+    @Test
+    public void playerWith400Bets400WinsBlackjackThenBalanceIs1_000() throws Exception {
+        Game game = new Game();
+        game.playerDeposits(400);
+        game.playerBets(400);
+
+        game.playerWinsBlackjack();
+
+        assertThat(game.playerBalance())
+                .isEqualTo(1_000);
+    }
 
 }
 
